cb421dcf6ff52642fd663947eac8a40873e12281
NNQA <anhla12h@gmail.com>
2024-06-27 21:17:28 +0700

    feat(auth): modify email not match

--- src/main/java/com/quocanh/doan/Controller/authentication/AuthenticationController.java
+++ src/main/java/com/quocanh/doan/Controller/authentication/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.quocanh.doan.Controller.authentication;
 
 
+import com.quocanh.doan.Exception.Signin.InvalidCredenticalException;
 import com.quocanh.doan.Security.Jwt.TokenProvider;
 import com.quocanh.doan.Service.ImplementService.User.UserPrincipal;
 import com.quocanh.doan.Service.ImplementService.User.UserRefreshTokenService;
@@ -11,10 +12,13 @@ import com.quocanh.doan.dto.request.authentication.LoginRequest;
 import com.quocanh.doan.dto.request.authentication.SignupRequest;
 import com.quocanh.doan.dto.response.LoginResponse;
 import com.quocanh.doan.dto.response.UserResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +34,7 @@ public class AuthenticationController {
 
     private final UserService userService;
     private final UserRefreshTokenService userRefreshTokenService;
+    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
     public AuthenticationController(AuthenticationManager authenticationManager,
                                     UserService userService,TokenProvider provider,
@@ -47,6 +52,8 @@ public class AuthenticationController {
     }
     @PostMapping("/login")
     public ResponseEntity Login(@RequestBody LoginRequest loginRequest) {
+        try {
+            logger.info("############## /api/auth/login started");
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
             SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -59,6 +66,11 @@ public class AuthenticationController {
                             String.valueOf(refreshToken)
                     )
             );
+        } catch (AuthenticationException exception) {
+            logger.info("-------------- increnditails login");
+            throw new InvalidCredenticalException("Some attributes is not match");
+        }
+
     }
 
     @PostMapping("/verifycode")
