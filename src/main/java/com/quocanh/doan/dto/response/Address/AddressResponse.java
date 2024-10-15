5c30a8a2a41b44b7e8c3e66e4cc642a419fb536e
NNQA <anhla12h@gmail.com>
2024-10-16 06:33:06 +0700

    change property of DTO Response

--- src/main/java/com/quocanh/doan/dto/response/Address/AddressResponse.java
+++ src/main/java/com/quocanh/doan/dto/response/Address/AddressResponse.java
@@ -1,2 +1,22 @@
-package com.quocanh.doan.dto.response.Address;public class AddressResponse {
+package com.quocanh.doan.dto.response.Address;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AddressResponse {
+    protected String provinceName;
+
+    protected String communeName;
+
+    protected String formattedAddressName;
+
+    protected float lat;
+
+    protected float lng;
+    protected String districtName;
 }
--- src/main/java/com/quocanh/doan/dto/response/Job/JobResponse.java
+++ src/main/java/com/quocanh/doan/dto/response/Job/JobResponse.java
@@ -1,5 +1,11 @@
 package com.quocanh.doan.dto.response.Job;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.quocanh.doan.Model.Address;
+import com.quocanh.doan.Model.JobCategory;
+import com.quocanh.doan.Model.Skill;
+import com.quocanh.doan.dto.request.AddressRequest;
+import com.quocanh.doan.dto.response.Address.AddressResponse;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -16,11 +22,16 @@ public class JobResponse {
     String title;
     String description;
     PositionResponse position;
-    JobTypeResponse jobType;
-    List<SkillReponse> skills;
+    JobTypeResponse type;
+    List<Skill> skills;
+    List<JobCategory> categories;
     String salary;
     String experience;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'hh:mm:ss")
     LocalDateTime expiredDate;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'hh:mm:ss")
     LocalDateTime createdDateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'hh:mm:ss")
     LocalDateTime updatedDateTime;
+    AddressResponse address;
 }
