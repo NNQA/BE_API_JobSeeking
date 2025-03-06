a0313ea58901561e407095d8c8330387436f8318
NNQA <anhla12h@gmail.com>
2025-03-06 17:16:23 +0700

    style: fix space and import package

--- src/main/java/com/quocanh/doan/Security/ConfigLucene/CustomAnalyzerConfigurer.java
+++ src/main/java/com/quocanh/doan/Security/ConfigLucene/CustomAnalyzerConfigurer.java
@@ -9,8 +9,10 @@ import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilterFactory;
 import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurationContext;
 import org.hibernate.search.backend.lucene.analysis.LuceneAnalysisConfigurer;
+import org.springframework.stereotype.Component;
 
-public class CustomAnalysisConfigurer implements LuceneAnalysisConfigurer {
+@Component
+public class CustomAnalyzerConfigurer implements LuceneAnalysisConfigurer {
 
     @Override
     public void configure(LuceneAnalysisConfigurationContext context) {
@@ -19,16 +21,14 @@ public class CustomAnalysisConfigurer implements LuceneAnalysisConfigurer {
                 .tokenFilter(LowerCaseFilterFactory.class)
                 .tokenFilter(ASCIIFoldingFilterFactory.class)
                 .tokenFilter(StopFilterFactory.class)
-                .param("stopwords", "vi")
+                .param("words", "stopwords_vi.txt")
                 .tokenFilter(WordDelimiterGraphFilterFactory.class);
 
         context.analyzer("englishAnalyzer").custom()
                 .tokenizer(StandardTokenizerFactory.class)
                 .tokenFilter(LowerCaseFilterFactory.class)
                 .tokenFilter(StopFilterFactory.class)
-                .param("stopwords", "english")
                 .tokenFilter(EnglishPossessiveFilterFactory.class)
                 .tokenFilter(PorterStemFilterFactory.class);
-
     }
 }
